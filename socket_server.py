509a109a6a7e6926e02d06da94cef95983903912
정우철 <oocheol@naver.com>
2025-02-07 16:04:07 +0900

    socket test : success

--- socket_server.py
+++ socket_server.py
@@ -2,7 +2,7 @@ import socket
 
 # 서버 설정
 host = "0.0.0.0"  # 서버의 IP 주소 또는 도메인 이름
-port = 12346  # 다른 포트 번호로 변경
+port = 12345  # 다른 포트 번호로 변경
 
 # 서버 소켓 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,29 +17,32 @@ while True:
     print(f"클라이언트 {client_address}가 연결되었습니다.")
 
     try:
-        # 클라이언트로부터 요청 받기
-        data = client_socket.recv(1024).decode("utf-8")
-        if not data:
-            continue
+        while True:  # 클라이언트와의 연결을 유지
+            # 클라이언트로부터 요청 받기
+            data = client_socket.recv(1024).decode("utf-8")
+            if not data:
+                print("클라이언트가 연결을 종료했습니다.")
+                break  # 연결 종료
 
-        # 요청 파싱
-        parts = data.split("&&")
-        if len(parts) != 0:
-            name = parts[0]
-            message = parts[1]
-            response = f"어서와! {name}"
+            # 요청 파싱
+            parts = data.split("&&")
+            if len(parts) == 2:
+                name = parts[0]
+                message = parts[1]
+                response = f"어서와! {name}"
 
-            # 클라이언트 이름과 메시지 출력
-            print(f"클라이언트 이름: {name}")
-            print(f"클라이언트 메시지: {message}")
-        else:
-            response = "유효하지 않은 요청"
+                # 클라이언트 이름과 메시지 출력
+                print(f"{name} : {message}")
+            else:
+                response = "유효하지 않은 요청"
 
-        # 응답 클라이언트에게 전송
-        client_socket.send(response.encode("utf-8"))
+            # 응답 클라이언트에게 전송
+            client_socket.send(response.encode("utf-8"))
 
     except Exception as e:
         print(f"오류 발생: {e}")
+        response = "서버에서 오류가 발생했습니다."
+        client_socket.send(response.encode("utf-8"))
 
     finally:
         # 클라이언트 소켓 닫기
